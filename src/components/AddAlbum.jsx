87ce0a8cc10e78c7f5cb302f39e0d0f28d8097e5
AdiFischer <adifischer1@gmail.com>
2022-11-01 17:22:20 -0400

    minor changes

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,26 +1,26 @@
 import { useState } from "react"
 
-export default function AddAlbum({setToggle, toggle}){// passing a func from parent = prop
+export default function AddAlbum({setToggle, toggle}){ // passing a function from parent = prop
     const [album, setAlbum] = useState('')
     const [artist, setArtist] = useState('')
     const [year, setYear] = useState(1970)
     const handleSubmit = (e) => {
         e.preventDefault()
         //lets check to see that they entered all data
-        if(!album || !artist || !year) {
+        if(!album || !artist || !year) { // form validation
             alert('fill-up')
             return
         }
-        const newAlbum = {artist, album, year}
-        fetch('https://albums-api-af.web.app/albums', {
-            method: 'POST',
+        const newAlbum = {artist, album, year}// taking 3 var and creating a new album
+        fetch('https://albums-api-af.web.app/albums', { 
+            method: 'POST', // try send data to get req. gives us the list
             headers: {
-                'Contact-Type': 'application/json'
+                'Content-Type': 'application/json' // done automatically, but here we need to tell it that we sending in json
             },
             body: JSON.stringify(newAlbum)
         })
         .then( () => {
-        //assume it works... (no .second then)
+        //assume it works... (no second .then)
         setToggle(!toggle)
         setAlbum('')
         setArtist('')
@@ -34,7 +34,7 @@ export default function AddAlbum({setToggle, toggle}){// passing a func from par
             <h3>Add New Album</h3>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="album">Album:
-                <input type="text" name="album" required
+                <input type="text" name="album" required // asking the browser to listen
                 onChange={e => setAlbum(e.target.value)}
                  value={album} />
                 </label> <br />
--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from "react";
 import AlbumCard from './AlbumCard'
 
 export default function AlbumList({toggle}) {
-    const [albums, setAlbums] = useState()
+    const [albums, setAlbums] = useState()// when we get the list we have somewhere to put it
     useEffect(() => {
         fetch('https://albums-api-af.web.app/albums')
         .then(response => response.json())
         .then(setAlbums)
         .catch(alert)
-    }, [toggle])
-    return(
+    }, [toggle])// []run once. run when a change made
+    return(// do the return first and then the new data will come up 
         <main className="album-list">
             {!albums
             ? <p>Loading...</p>
